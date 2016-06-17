634f1b1dbd81c715909ff8f406293915ea650d42
Zak Henry <zak.henry@gmail.com>
2016-06-17 13:51:19 +0100

    fix(test): Drop redundant comment in test http store

--- src/browser/stores/http.store.spec.ts
+++ src/browser/stores/http.store.spec.ts
@@ -17,17 +17,7 @@ class TestModel extends Model {
 class TestHttpStore extends HttpStore<TestModel> {
 
   constructor(http: Http, loggerBase: Logger) {
-    /**
-     * @todo resolve why the <any>http is needed to suppress the following error:
-     *
-     * src/browser/stores/user.http.store.ts(13,17): error TS2345: Argument of type 'Http' is not
-     *   assignable to parameter of type 'Http'. Property '_backend' is protected but type 'Http'
-     *   is not a class derived from 'Http'.
-     *
-     * It may have something to do with both ubiquits/core and the current project
-     * depending on angular so TS thinks they could be different implementations?
-     */
-    super(TestModel, <any>http, loggerBase);
+    super(TestModel, http, loggerBase);
   }
 
   protected endpoint(id?: string): string {
