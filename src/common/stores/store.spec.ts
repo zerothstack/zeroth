93dbaa26c058896c1f8b88fade08ef359f34e943
Zak Henry <zak.henry@gmail.com>
2016-06-22 12:16:59 +0100

    feat(injectable custom validation):Implement container registration so custom validators can use injectable dependencies.
    Closes #84

--- src/common/stores/store.spec.ts
+++ src/common/stores/store.spec.ts
@@ -1,40 +1,73 @@
 import { it, inject, beforeEachProviders, expect, describe } from '@angular/core/testing';
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { MockStore } from './mock.store';
 import { Model, identifier } from '../models/model';
 import { Store } from './store';
 import { primary } from '../types/primary.decorator';
-import { MinLength } from '../validation';
+import { MinLength, Validate, ValidatorConstraint } from '../validation';
 import { ValidationException } from '../../server/exeptions/exceptions';
+import { ValidatorInterface } from 'class-validator';
+import Spy = jasmine.Spy;
+
+@Injectable()
+class StubService {
+
+  public isTruthy(value: any): boolean {
+    return !!value;
+  }
+
+}
+
+@Injectable()
+@ValidatorConstraint()
+class CustomValidator implements ValidatorInterface {
+
+  constructor(protected service: StubService) {
+  }
+
+  public validate(value: any): boolean {
+
+    return this.service.isTruthy(value);
+  }
+
+}
 
 class Ship extends Model {
   @primary
-  public shipId:number;
+  public shipId: number;
 
   @MinLength(2)
-  public name:string;
+  public name: string;
 
-  public idRandom:string;
-  public idSeeded:string;
+  public idRandom: string;
+  public idSeeded: string;
+
+  @Validate(CustomValidator)
+  public truthyValue: any;
+
 }
 
 abstract class ShipStore extends Store<Ship> {
 
 }
 
-
+@Injectable()
 class ShipMockStore extends MockStore<Ship> {
 
-  constructor() {
-    super(Ship);
+  constructor(injector: Injector) {
+    super(Ship, injector);
   }
 
-  protected getMock(id?:identifier):Ship {
+  protected getMock(id?: identifier): Ship {
     return new this.modelStatic({
-      shipId: id || this.chance().integer(),
+      shipId: id || this.chance()
+        .integer(),
       name: 'Razorback',
-      idRandom: this.chance().guid(),
-      idSeeded: this.chance(1).guid(),
+      idRandom: this.chance()
+        .guid(),
+      idSeeded: this.chance(1)
+        .guid(),
+      truthyValue: true
     });
   }
 
@@ -43,14 +76,20 @@ class ShipMockStore extends MockStore<Ship> {
 @Injectable()
 class TestClass {
 
-  constructor(public shipStore:ShipStore){
+  constructor(public shipStore: ShipStore) {
 
   }
 
 }
 
-const providers = [
+//we need to have a singleton instance here to spy on, as the validator creates new singletons
+// of the custom validator
+const stubServiceSingleton = new StubService();
+
+const providers:any[] = [
   TestClass,
+  CustomValidator,
+  {provide: StubService, deps: [], useFactory: () => stubServiceSingleton},
   {provide: ShipStore, useClass: ShipMockStore},
 ];
 
@@ -60,90 +99,128 @@ describe('Mock Store', () => {
 
   it('is injected with the store instance token', inject([TestClass], (c: TestClass) => {
 
-    expect(c instanceof TestClass).toBe(true);
-    expect(c.shipStore instanceof Store).toBe(true);
-    expect(c.shipStore instanceof ShipMockStore).toBe(true);
+    expect(c instanceof TestClass)
+      .toBe(true);
+    expect(c.shipStore instanceof Store)
+      .toBe(true);
+    expect(c.shipStore instanceof ShipMockStore)
+      .toBe(true);
 
   }));
 
   it('retrieves a promise of a mock entity', inject([TestClass], (c: TestClass) => {
 
-    return c.shipStore.findOne(1234).then((entity) => {
+    return c.shipStore.findOne(1234)
+      .then((entity) => {
 
-      expect(entity instanceof Ship).toBe(true);
-      expect(entity.getIdentifier()).toBe(1234);
-    });
+        expect(entity instanceof Ship)
+          .toBe(true);
+        expect(entity.getIdentifier())
+          .toBe(1234);
+      });
 
   }));
 
   it('mocks random and seeded random data', inject([TestClass], (c: TestClass) => {
 
-    return c.shipStore.findMany(1234).then((ships:Ship[]) => {
+    return c.shipStore.findMany(1234)
+      .then((ships: Ship[]) => {
 
-      expect(ships[0].idRandom).not.toEqual(ships[1].idRandom);
-      expect(ships[0].idSeeded).toEqual(ships[1].idSeeded);
+        expect(ships[0].idRandom)
+          .not
+          .toEqual(ships[1].idRandom);
+        expect(ships[0].idSeeded)
+          .toEqual(ships[1].idSeeded);
 
-    });
+      });
 
   }));
 
   it('mocks save action', inject([TestClass], (c: TestClass) => {
 
-    let shipRef:Ship = null;
+    let shipRef: Ship = null;
 
     return c.shipStore.findOne(1234)
-      .then((ship:Ship) => {
+      .then((ship: Ship) => {
         shipRef = ship;
         return c.shipStore.saveOne(ship)
       })
       .then((ship) => {
-        expect(shipRef).toEqual(ship);
+        expect(shipRef)
+          .toEqual(ship);
       });
 
   }));
 
   it('explicitly hydrates from raw data', inject([TestClass], (c: TestClass) => {
 
-    let data:any = {
+    let data: any = {
       shipId: 321,
       name: 'Rocinante'
     };
 
     return c.shipStore.hydrate(data)
-      .then((ship:Ship) => {
-        expect(ship instanceof Ship).toBe(true);
-        expect(ship.getIdentifier()).toEqual(321);
-        expect(ship.name).toEqual(data.name);
+      .then((ship: Ship) => {
+        expect(ship instanceof Ship)
+          .toBe(true);
+        expect(ship.getIdentifier())
+          .toEqual(321);
+        expect(ship.name)
+          .toEqual(data.name);
       })
 
   }));
 
   it('validates model data, returning instance on pass', inject([TestClass], (c: TestClass) => {
-    let shipRef:Ship = null;
+    let shipRef: Ship = null;
 
     return c.shipStore.findOne(1234)
-      .then((ship:Ship) => {
+      .then((ship: Ship) => {
         shipRef = ship;
         return c.shipStore.validate(ship);
-      }).then((ship) => {
-        expect(shipRef).toEqual(ship);
+      })
+      .then((ship) => {
+        expect(shipRef)
+          .toEqual(ship);
       });
 
   }));
 
   it('validates model data, returning ValidationException with data on failure', inject([TestClass], (c: TestClass) => {
     return c.shipStore.findOne(1234)
-      .then((ship:Ship) => {
+      .then((ship: Ship) => {
         ship.name = 'a'; //too short
         return c.shipStore.validate(ship);
       })
       .catch((e) => {
-        expect(e instanceof ValidationException).toBe(true);
-        expect(e.getData()[0].errorName).toEqual('min_length');
+        expect(e instanceof ValidationException)
+          .toBe(true);
+        expect(e.getData()[0].errorName)
+          .toEqual('min_length');
       });
 
   }));
 
+  it('validates model data, with custom validation with injected service dependency', inject([TestClass, StubService], (c: TestClass, s: StubService) => {
 
+    let customValidatorServiceSpy = spyOn(stubServiceSingleton, 'isTruthy')
+      .and
+      .callThrough();
+
+    return c.shipStore.findOne(1234)
+      .then((ship: Ship) => {
+
+        ship.truthyValue = 'truthy';
+
+        return c.shipStore.validate(ship);
+      })
+      .then((ship) => {
+
+        expect(customValidatorServiceSpy)
+          .toHaveBeenCalledWith('truthy');
+
+      });
+
+  }));
 
 });
--- src/common/validation/index.ts
+++ src/common/validation/index.ts
@@ -3,3 +3,4 @@
 // of the MetadataStorage class which is used as a singleton. Otherwise, the models
 // may register their validations against a different store than what is validated with
 export * from 'class-validator';
+export {default as validator} from 'class-validator';
